671faa4ef9d69ccfe0c6309d9365395242a112fe
selin <selinshere@gmail.com>
2023-07-05 17:22:59 +0300

    textare, addNote

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 const recordButton = document.querySelector(".recordButton");
 const recordContainer = document.querySelector(".record");
 const todos = document.querySelector(".todos");
+const voiceNote = document.querySelector("#note");
 
 function record() {
   recordButton.innerText = "recording...";
   var recognition = new webkitSpeechRecognition();
   recognition.lang = "tr-TR";
   recognition.onresult = function (event) {
-    console.log(event.results[0][0].transcript);
+    voiceNote.value = event.results[0][0].transcript;
+    // console.log(event.results[0][0].transcript);
+  };
+  recordButton.innerHTML = `
+  Record <i class="fa-solid fa-microphone"></i>
+  `;
+  recognition.start();
+}
+
+function addNote() {
     todos.innerHTML += `
      <li>
-     ${event.results[0][0].transcript}
+     ${voiceNote.value}
      <button><i class="fa-solid fa-check"></i></button>
      </li>
       `;
-      recordButton.innerHTML = `
-      Record <i class="fa-solid fa-microphone"></i>
-      `;
-  };
-  recognition.start();
+      voiceNote.value = "";
+      window.alert("New note is adding!")
+ 
 }
